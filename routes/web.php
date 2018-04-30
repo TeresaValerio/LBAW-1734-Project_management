9e43632d38d3b589076310e34826d83d0c93492a
teresa <up201405655@fe.up.pt>
2018-04-30 13:10:07 +0100

    Changes to "change password"

--- routes/web.php
+++ routes/web.php
@@ -36,7 +36,7 @@ Route::post('/loginme','Auth\LoginController@login');
 
 Route::post('/register','Auth\RegisterController@register');
 
-Route::post('/changePassword','settingsController@change');
+Route::post('/changePassword','SettingsController@change');
 
 Auth::routes();
 
